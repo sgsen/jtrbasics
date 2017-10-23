e37b8d60942ebb82d2cda5693eff5fb3782fb969
sgsen <sohamgsen@gmail.com>
2017-10-23 19:47:41 +0530

    Updated load_payments_data() for 2nd half Oct - take 3

--- R/jt_basics_package.R
+++ R/jt_basics_package.R
@@ -232,7 +232,7 @@ load_payments_data <- function(){
   ##A. Get monthly sheets
   pd_oct2 <- getmonthlysheet("PaymentReconciliationData_Oct17_18_31")
   
-  pd_oct1 <- getmonthlysheet("PaymentReconciliationData_Oct17_01_20")
+  pd_oct1 <- getmonthlysheet("PaymentReconciliationData_Oct17_01_17")
   paymentsdata <- rbind(pd_oct2, pd_oct1)
   
   pd_sept2 <- getmonthlysheet("PaymentReconciliationData_Sep17_22_30")
